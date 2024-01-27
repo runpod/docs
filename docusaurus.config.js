39799f8e8fb478598be3706af71fec71258c7023
Allyson Rosenthal <allyson_rosenthal@utexas.edu>
2024-01-27 16:17:34 -0600

    Fixes Edit this Page links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           routeBasePath: "",
-          editUrl: "https://github.com/runpod/docs",
+          editUrl: "https://github.com/runpod/docs/blob/main",
         },
         /*
         blog: {
