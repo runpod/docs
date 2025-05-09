30b7ab9e9b7e105e95d01b53482da5a7341a0737
Mo King <muhsinking@gmail.com>
2025-05-09 11:57:35 -0400

    Documentation edits made through Mintlify web editor

--- kapa.js
+++ kapa.js
@@ -5,4 +5,6 @@ script.setAttribute("data-website-id", "5b21ac2d-dd52-41f2-b02b-cd82467e45cc");
 script.setAttribute("data-project-name", "RunPod");
 script.setAttribute("data-project-color", "#070D27");
 script.setAttribute("data-project-logo", "https://raw.githubusercontent.com/runpod/docs/refs/heads/mintlify-migration/Color.svg",);
+script.setAttribute("data-button-image-width", 24);
+script.setAttribute("data-button-image-height", 24);
 document.head.appendChild(script);
