55c9d2a51cc5594ba2bc7c31e12dca637eee9d53
Mo King <muhsinking@gmail.com>
2025-05-09 13:50:46 -0400

    Switch to square logo

--- kapa.js
+++ kapa.js
@@ -4,7 +4,7 @@ script.async = true;
 script.setAttribute("data-website-id", "5b21ac2d-dd52-41f2-b02b-cd82467e45cc");
 script.setAttribute("data-project-name", "RunPod");
 script.setAttribute("data-project-color", "#070D27");
-script.setAttribute("data-project-logo", "https://raw.githubusercontent.com/runpod/docs/refs/heads/mintlify-migration/Color.svg",);
+script.setAttribute("data-project-logo", "https://raw.githubusercontent.com/runpod/docs/refs/heads/mintlify-migration/icon_square.svg",);
 script.setAttribute("data-button-image-width", 30);
 script.setAttribute("data-button-image-height", 30);
 document.head.appendChild(script);
