a79b00a751df8e7e09f0fbd964411c5e475437bb
Mo King <muhsinking@gmail.com>
2025-05-09 11:01:12 -0400

    Documentation edits made through Mintlify web editor

--- kapa.js
+++ kapa.js
@@ -1,7 +1,8 @@
-src: "https://widget.kapa.ai/kapa-widget.bundle.js",
-"data-website-id": "d8e25089-cadd-4c1c-9010-7e83cd99a2a5",
-"data-project-name": "RunPod",
-"data-project-color": "#070D27",
-"data-project-logo":
-"https://avatars.githubusercontent.com/u/95939477?s=200&v=4",
-async: true,
+const script = document.createElement("script");
+script.src = "https://widget.kapa.ai/kapa-widget.bundle.js";
+script.async = true;
+script.setAttribute("data-website-id", "d8e25089-cadd-4c1c-9010-7e83cd99a2a5");
+script.setAttribute("data-project-name", "RunPod");
+script.setAttribute("data-project-color", "#070D27");
+script.setAttribute("data-project-logo", "https://avatars.githubusercontent.com/u/95939477?s=200&v=4",);
+document.head.appendChild(script);
