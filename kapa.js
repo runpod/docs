11899030a305aad001939a6933ced18ac9c11422
Mo King <muhsinking@gmail.com>
2025-06-02 12:41:21 -0400

    Update kapa

--- kapa.js
+++ kapa.js
@@ -2,7 +2,7 @@ const script = document.createElement("script");
 script.src = "https://widget.kapa.ai/kapa-widget.bundle.js";
 script.async = true;
 script.setAttribute("data-website-id", "5b21ac2d-dd52-41f2-b02b-cd82467e45cc");
-script.setAttribute("data-project-name", "RunPod");
+script.setAttribute("data-project-name", "Runpod");
 script.setAttribute("data-project-color", "#070D27");
 script.setAttribute("data-project-logo", "https://raw.githubusercontent.com/runpod/docs/refs/heads/main/og-icon-trim.svg",);
 // script.setAttribute("data-project-logo", "https://raw.githubusercontent.com/runpod/docs/refs/heads/mintlify-migration/icon_square.svg",);
