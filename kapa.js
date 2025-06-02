ab18c6007a99a1b2d1f5fdb5ed61c01e4b4ca723
Mo King <muhsinking@gmail.com>
2025-06-02 16:07:20 -0400

    Test fix

--- kapa.js
+++ kapa.js
@@ -2,12 +2,11 @@ const script = document.createElement("script");
 script.src = "https://widget.kapa.ai/kapa-widget.bundle.js";
 script.async = true;
 script.setAttribute("data-website-id", "5b21ac2d-dd52-41f2-b02b-cd82467e45cc");
-script.setAttribute("data-project-name", "Runpod");
+script.setAttribute("data-project-name", "RunPod");
 script.setAttribute("data-project-color", "#070D27");
 // script.setAttribute("data-project-logo", "https://raw.githubusercontent.com/runpod/docs/refs/heads/main/og-icon-trim.svg",);
 script.setAttribute("data-button-image-width", 30);
 script.setAttribute("data-button-image-height", 30);
-
 
 // v1
 // script.setAttribute("data-project-logo", "https://raw.githubusercontent.com/runpod/docs/51f96638ad16845f314e140d93ea55f33876fc43/logo/icon_square.svg",);
