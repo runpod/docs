4abf987e7e2904df80cfc44dbf25034e4f9f13ec
Mo King <muhsinking@gmail.com>
2025-06-02 15:54:07 -0400

    test purple version

--- kapa.js
+++ kapa.js
@@ -16,8 +16,6 @@ script.setAttribute("data-button-image-height", 30);
 // script.setAttribute("data-button-text-color", "#19181E");
 // script.setAttribute("data-button-text-shadow", "0px 0px 0px 0px");
 // script.setAttribute("data-button-border", "1px solid #E1E1E5");
-// script.setAttribute("data-modal-header-bg-color", "#5D29F0");
-// script.setAttribute("data-modal-title-color", "#FFFFFF");
 
 // v2
 script.setAttribute("data-project-logo", "https://raw.githubusercontent.com/runpod/docs/22268d7d3adb48de47dd22a72f554c9aff9b9971/logo/icon_square_white.svg",);
@@ -25,7 +23,7 @@ script.setAttribute("data-button-bg-color", "#5D29F0");
 script.setAttribute("data-button-hover-bg-color", "#AE6DFF");
 script.setAttribute("data-button-text-color", "#FFFFFF");
 script.setAttribute("data-button-text-shadow", "0px 0px 0px 0px");
-script.setAttribute("data-button-border", "1px solid #E1E1E5");
+script.setAttribute("data-button-border", "0px solid #E1E1E5");
 // script.setAttribute("data-button-hover-border", "1px solid rgb(7, 7, 7)");
 script.setAttribute("data-modal-header-bg-color", "#5D29F0");
 script.setAttribute("data-modal-title-color", "#FFFFFF");
